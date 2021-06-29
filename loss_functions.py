59721334e170379aeb42152310b54a49598db489
Livi <piazza.olivia@gmail.com>
2021-06-28 20:31:36 -0700

    Added functionality. Removed extraneous comments

--- loss_functions.py
+++ loss_functions.py
@@ -1,5 +1,5 @@
 import torch
-import diff_operators
+import diff_operators, utils
 
 
 def image_mse(model_output, gt):
@@ -22,10 +22,24 @@ def image_mse_TV_prior(k1, model, model_output, gt):
                 rand_output['model_out'], rand_output['model_in']))).mean()}
 
 
+def image_mse_smoothing_TV_prior(k1, model, model_output, gt, data_shape):
+    coords_rand = 2 * (torch.rand((model_output['model_in'].shape[0],
+                                   model_output['model_in'].shape[1] // 2,
+                                   model_output['model_in'].shape[2])).cuda() - 0.5)
+    rand_input = {'coords': coords_rand}
+    rand_output = model(rand_input['coords'])
+    
+    model_output['model_out'] = utils.attach_tensor(utils.detach_tensor(model_output['model_out'], data_shape))
+
+    return {'img_loss': ((model_output['model_out'] - gt['coords']) ** 2).mean(),
+            'prior_loss': k1 * (torch.abs(diff_operators.gradient(
+                rand_output['model_out'], rand_output['model_in']))).mean()}
+
+
 def image_mse_FH_prior(k1, model, model_output, gt):
     coords_rand = 2 * (torch.rand((model_output['model_in'].shape[0],
                                    model_output['model_in'].shape[1] // 2,
-                                   model_output['model_in'].shape[2])).cuda() - 0.5) # torch.Size([1, 91014, 1])
+                                   model_output['model_in'].shape[2])).cuda() - 0.5) 
     rand_input = {'coords': coords_rand}
     rand_output = model(rand_input['coords'])
 
