0b9b56fe9ec08a1379b98f28289adf34c1c7da62
Livi <piazza.olivia@gmail.com>
2021-07-06 21:27:51 -0700

    updated to include statistical weighting

--- loss_functions.py
+++ loss_functions.py
@@ -22,18 +22,17 @@ def image_mse_TV_prior(k1, model, model_output, gt):
                 rand_output['model_out'], rand_output['model_in']))).mean()}
 
 
-def image_mse_smoothing_TV_prior(k1, model, model_output, gt, data_shape):
+def image_weighted_mse_TV_prior(k1, model, model_output, gt, weight):
     coords_rand = 2 * (torch.rand((model_output['model_in'].shape[0],
                                    model_output['model_in'].shape[1] // 2,
                                    model_output['model_in'].shape[2])).cuda() - 0.5)
     rand_input = {'coords': coords_rand}
     rand_output = model(rand_input['coords'])
-    
-    model_output['model_out'] = utils.attach_tensor(utils.detach_tensor(model_output['model_out'], data_shape))
 
-    return {'img_loss': ((model_output['model_out'] - gt['coords']) ** 2).mean(),
+    return {'img_loss': (weight * (model_output['model_out'] - gt['coords']) ** 2).mean(),
             'prior_loss': k1 * (torch.abs(diff_operators.gradient(
                 rand_output['model_out'], rand_output['model_in']))).mean()}
+
 
 
 def image_mse_FH_prior(k1, model, model_output, gt):
