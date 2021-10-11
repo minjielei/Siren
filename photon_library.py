5d6cae211d96b3a54f602b6172d0043780604bf9
minjielei <minjie5p@gmail.com>
2021-10-10 17:12:44 -0700

    fix plib coord and data mismatch

--- photon_library.py
+++ photon_library.py
@@ -18,16 +18,25 @@ class PhotonLibrary(object):
             self._min  = np.array(f['min'])
             self._max  = np.array(f['max'])
             self.shape = np.array(f['numvox'])
+            
+        pmt_data = np.loadtxt('pmt_loc.csv',skiprows=1,delimiter=',')
+        if not (pmt_data[:,0].astype(np.int32) == np.arange(pmt_data.shape[0])).all():
+            raise Exception('pmt_loc.csv contains optical channel data not in order of channel numbers')
+        self._pmt_pos = pmt_data[:,1:4]
+        self._pmt_dir = pmt_data[:,4:7]
+        if not self._pmt_pos.shape[0] == self._vis.shape[1]:
+            raise Exception('Optical detector count mismatch: photon library %d v.s. pmt_loc.csv %d' % (self._vis.shape[1],
+                                                                                                        self._pmt_pos.shape[0])
+                           )
+        if ((self._pmt_pos < self._min) | (self._max < self._pmt_pos)).any():
+            raise Exception('Some PMT positions are out of the volume bounds')
+        # Convert the PMT positions in a normalized coordinate (fractional position within the voxelized volume)
+#         self._pmt_pos = (self._pmt_pos - self._min) / (self._max - self._min)
 
     def numpy(self):
-        x = np.linspace(0, 1, self.shape[0], endpoint=False)
-        y = np.linspace(0, 1, self.shape[1], endpoint=False)
-        z = np.linspace(0, 1, self.shape[2], endpoint=False)
-
-        coordx, coordy, coordz = np.meshgrid(x, y, z)
-        coord = np.reshape(np.stack([coordx, coordy, coordz], -1), (-1, 3))
-        axis = (coord * self.shape).astype(int)
-        data = self.VisibilityFromAxisID(axis)
+        axis = self.VoxID2AxisID(np.arange(self._vis.shape[0]))
+        coord = (axis + 0.5) / self.shape
+        data = self.VisibilityFromAxisID(axis.astype(int))
 
         return coord, data
 
@@ -62,7 +71,7 @@ class PhotonLibrary(object):
 
     def VisibilityFromXYZ(self, pos, ch=None):
         if not torch.is_tensor(pos):
-          pos = torch.tensor(pos, device=device)
+            pos = torch.tensor(pos, device=device)
         return self.Visibility(self.Position2VoxID(pos), ch)
 
     def Visibility(self, vids, ch=None):
@@ -110,3 +119,99 @@ class PhotonLibrary(object):
         axis_ids = ((pos - self._min) / (self._max - self._min) * self.shape).int()
 
         return (axis_ids[:, 0] + axis_ids[:, 1] * self.shape[0] +  axis_ids[:, 2]*(self.shape[0] * self.shape[1])).long()
+    
+    def VoxID2AxisID(self, vid):
+        '''
+        Takes a voxel ID and converts to discretized index along xyz axis
+        INPUT
+          vid - The voxel ID (single integer)          
+        RETURN
+          Length 3 integer array noting the position in discretized index along xyz axis
+        '''
+        xid = vid.astype(int) % self.shape[0]
+        yid = ((vid - xid) / self.shape[0]).astype(int) % self.shape[1]
+        zid = ((vid - xid - (yid * self.shape[0])) / (self.shape[0] * self.shape[1])).astype(int) % self.shape[2]
+        
+        return np.reshape(np.stack([xid,yid,zid], -1), (-1, 3)).astype(np.float32) 
+    
+    def Visibility2D(self, axis, frac, gt, pred, ch=None):
+        '''
+        Provides a 2D slice of a visibility map at a fractional location along the specified axis
+        INPUT
+          axis - One of three cartesian axis 'x', 'y', or 'z'
+          frac - A floating point value in the range [0,1] to specify the location, in fraction, along the axis
+          ch   - An integer or a list of integers specifying a (set of) optical detectors (if not provided, visibility for all detectors are summed)
+        RETURN
+          2D (XY) slice of a visibility map
+        '''
+        axis_labels = ['x','y','z']
+        ia, ib, itarget = 0,0,0
+        if   axis == 'x': itarget, ia, ib = 0,1,2
+        elif axis == 'y': itarget, ia, ib = 1,2,0
+        elif axis == 'z': itarget, ia, ib = 2,0,1
+        else:
+            print('axis must be x, y, or z')
+            raise ValueError
+        
+        if frac < 0 or 1.0 < frac:
+            print('frac must be between 0.0 and 1.0')
+            raise ValueError
+            
+        loc_target = int(float(frac) * self.shape[itarget] + 0.5)
+        result_gt = np.zeros(shape=[self.shape[ia],self.shape[ib]],dtype=np.float32)
+        result_pred = np.zeros(shape=[self.shape[ia],self.shape[ib]],dtype=np.float32)
+        axis_id = [0,0,0]
+        chs = np.arange(len(self._vis[0]))
+        if ch is not None:
+            chs = [ch] if type(ch) == type(int()) else ch
+        for loc_a in range(self.shape[ia]):
+            for loc_b in range(self.shape[ib]):
+                axis_id[itarget] = loc_target
+                axis_id[ia]      = loc_a
+                axis_id[ib]      = loc_b
+                vid = self.AxisID2VoxID(np.array([axis_id]))[0]
+                for ch in chs:
+                    result_gt[loc_a][loc_b] += gt[vid][ch]
+                    result_pred[loc_a][loc_b] += pred[vid][ch]
+        return [result_gt, result_pred]
+    
+    def PlotVisibility2D(self,axis,frac,gt,pred,ch=None):
+        '''
+        Visualize a 2D slice of a visibility map at a fractional location along the specified axis
+        INPUT
+          axis - One of three cartesian axis 'x', 'y', or 'z'
+          frac - A floating point value in the range [0,1] to specify the location, in fraction, along the axis
+          ch   - An integer or a list of integers specifying a (set of) optical detectors (if not provided, visibility for all detectors are summed)
+        RETURN
+          figure object
+        '''
+        axis_labels = ['x','y','z']
+        ia, ib = 0,0
+        if   axis == 'x': ia, ib = 1,2
+        elif axis == 'y': ia, ib = 2,0
+        elif axis == 'z': ia, ib = 0,1
+        else:
+            print('axis must be x, y, or z')
+            raise ValueError
+            
+        ar=self.Visibility2D(axis,frac,gt,pred,ch)
+        pos_range=np.column_stack([self._min,self._max])
+        extent = np.concatenate([pos_range[ib], pos_range[ia]])
+        
+        import matplotlib.pyplot as plt
+        import matplotlib as mpl
+        
+        fig,ax=plt.subplots(2, 1, figsize=(16,6), sharex=True,sharey=True,facecolor='w', constrained_layout=True)
+        color_map = plt.cm.get_cmap('viridis')
+        reversed_color_map = color_map.reversed()
+        for i in range(2):
+            ar[i] = - np.log(ar[i] + 1e-7)
+            im = ax[i].matshow(ar[i],extent=extent, cmap=reversed_color_map, vmin = np.min(ar[0]), vmax = np.max(ar[0]))
+            ax[i].tick_params(labelsize=16,bottom=True,top=False,left=True,right=False,labelleft=True,labelbottom=True)
+            ax[i].set_ylabel('%s [cm]' % axis_labels[ia].upper(),fontsize=20)
+            
+        cbar = fig.colorbar(im, ax=ax.ravel().tolist(), pad=0.02)
+        cbar.ax.tick_params(labelsize=14)
+       
+        ax[1].set_xlabel('%s [cm]' % axis_labels[ib].upper(),fontsize=20)
+        return fig 
